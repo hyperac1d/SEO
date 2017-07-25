52a1e7908c09d7b6457a862a375ab32a76e1c2ee
Marco Mat <Marco Mat>
2017-07-26 01:30:04 +0800

    ninth commit

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,26 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $(".dropdown-button").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+// $(function() {
+// 	// Set this variable with the height of your sidebar + header
+// 	var offsetPixels =  780;
+
+// 	$(window).scroll(function() {
+// 		if ($(window).scrollTop() > offsetPixels) {
+// 			$( ".sidebarBox" ).css({
+// 				"position": "fixed",
+// 				"top": "15px"
+// 			});
+// 		} else {
+// 			$( ".sidebarBox" ).css({
+// 				"position": "static"
+// 			});
+// 		}
+// 	});
+// });
