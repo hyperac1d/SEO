4d3d6da56e65657012ecdef97123d87674766cd3
gelo <ngelmat@yahoo.com>
2017-07-26 23:05:45 +0800

    sticky and scrollspy

--- public/js/init.js
+++ public/js/init.js
@@ -4,6 +4,34 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
+
+    $('.scrollspy').scrollSpy();    
+    
+    
+    $('#home,#history,#methods,#getting_indexed,#preventing_crawling,#increasing_prominence').hover(function(){
+      $('li.active').removeClass('active');
+      $('.active').removeClass('active'); 
+       $('.nav-menu-button li').removeClass('active');
+       $('#' + $(this).attr('id') + 'Li').addClass('active');
+       if (["getting_indexed", "preventing_crawling", "increasing_prominence"].indexOf($(this).attr('id')) > -1) {
+         $('#dropdown1 li').removeClass('active');
+         if($('#' + $(this).attr('id') +  'Li').parent().parent().attr('id') === 'methodsLi') {
+           $('.nav-menu-button li').removeClass('active');
+           $('#methodsLi').addClass('active');    
+           $('#' + $(this).attr('id') + 'Li').addClass('active');
+         } else {
+           $('.nav-menu-button li').removeClass('active');
+         }
+       }
+       
+       
+    });
+    
+    
+    $('li#homeLi').click(function() {
+      location.href = location.protocol + "//" + location.host;
+    });
+    
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
